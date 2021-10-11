567a102802c30e387c9091333b43aab79d7f470a
Sebastian Aguilar <sebaguilar03@gmail.com>
2021-10-11 20:36:11 -0300

    Update js.js

--- js/js.js
+++ js/js.js
@@ -2,7 +2,7 @@ popupWhatsApp = () => {
     let btnClosePopup = document.querySelector('.closePopup');
     let btnOpenPopup = document.querySelector('.whatsapp-button');
     let popup = document.querySelector('.popup-whatsapp');
-
+    let sendBtn = document.getElementById('send-btn');
 
 
 
