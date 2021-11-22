0fd6cba83d4d4eccb3b0eba0b71d70e46df0b35e
Sebastian Aguilar <sebaguilar03@gmail.com>
2021-11-22 15:43:34 -0300

    xd

--- js/js.js
+++ js/js.js
@@ -1,40 +1,40 @@
 $(document).ready(function(){
-// AGREGANDO CLASE ACTIVE AL PRIMER ENLACE ====================
-$('.category_list .category_item[category="all"]').addClass('ct_item-active');
 
-// FILTRANDO PRODUCTOS  ============================================
+	// AGREGANDO CLASE ACTIVE AL PRIMER ENLACE ====================
+	$('.category_list .category_item[category="all"]').addClass('ct_item-active');
 
-$('.category_item').click(function(){
-  var catProduct = $(this).attr('category');
-  console.log(catProduct);
+	// FILTRANDO PRODUCTOS  ============================================
 
-  // AGREGANDO CLASE ACTIVE AL ENLACE SELECCIONADO
-  $('.category_item').removeClass('ct_item-active');
-  $(this).addClass('ct_item-active');
+	$('.category_item').click(function(){
+		var catProduct = $(this).attr('category');
+		console.log(catProduct);
 
-  // OCULTANDO PRODUCTOS =========================
-  $('.product-item').css('transform', 'scale(0)');
-  function hideProduct(){
-    $('.product-item').hide();
-  } setTimeout(hideProduct,400);
+		// AGREGANDO CLASE ACTIVE AL ENLACE SELECCIONADO
+		$('.category_item').removeClass('ct_item-active');
+		$(this).addClass('ct_item-active');
 
-  // MOSTRANDO PRODUCTOS =========================
-  function showProduct(){
-    $('.product-item[category="'+catProduct+'"]').show();
-    $('.product-item[category="'+catProduct+'"]').css('transform', 'scale(1)');
-  } setTimeout(showProduct,400);
+		// OCULTANDO PRODUCTOS =========================
+		$('.product-item').css('transform', 'scale(0)');
+		function hideProduct(){
+			$('.product-item').hide();
+		} setTimeout(hideProduct,400);
+
+		// MOSTRANDO PRODUCTOS =========================
+		function showProduct(){
+			$('.product-item[category="'+catProduct+'"]').show();
+			$('.product-item[category="'+catProduct+'"]').css('transform', 'scale(1)');
+		} setTimeout(showProduct,400);
+	});
+
+	// MOSTRANDO TODOS LOS PRODUCTOS =======================
+
+	$('.category_item[category="all"]').click(function(){
+		function showAll(){
+			$('.product-item').show();
+			$('.product-item').css('transform', 'scale(1)');
+		} setTimeout(showAll,400);
+	});
 });
-
-// MOSTRANDO TODOS LOS PRODUCTOS =======================
-
-$('.category_item[category="all"]').click(function(){
-  function showAll(){
-    $('.product-item').show();
-    $('.product-item').css('transform', 'scale(1)');
-  } setTimeout(showAll,400);
-});
-});
-
 popupWhatsApp = () => {
   
     let btnClosePopup = document.querySelector('.closePopup');
